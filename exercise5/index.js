d3c8163a50def19f1466f1aed3bb0d66c5ac0881
lewisev <lewisev@udel.edu>
2023-12-12 19:33:45 -0500

    Firebase functionality, updated gitignore

--- exercise5/index.js
+++ exercise5/index.js
@@ -1,3 +1,11 @@
+var admin = require("firebase-admin");
+var serviceAccount = require("./lewisev-cisc474-exercises-firebase.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://lewisev-cisc474-exercises-default-rtdb.firebaseio.com"
+});
+
+
 const http = require('http')
 const port = 8080
 // Create a server object:
@@ -18,3 +26,20 @@ server.listen(port, function (error) {
         console.log('Server is listening on port' + port);
     }
 })
+
+
+// Get a database reference to our blog
+const { getDatabase } = require('firebase-admin/database');
+const db = getDatabase();
+const ref = db.ref('server/saving-data/');
+const usersRef = ref.child('users');
+usersRef.set({
+    alanisawesome: {
+        date_of_birth: 'June 23, 1912',
+        full_name: 'Alan Turing'
+    },
+    gracehop: {
+        date_of_birth: 'December 9, 1906',
+        full_name: 'Grace Hopper'
+    }
+});
